28dc966028f9ab10ee3284d8c2e7310b5f795d75
zura <zuramanagadze1@yahoo.com>
2025-06-24 12:23:01 +0400

    11-same site lax

--- src/lib/generateJWTAndSetCookies.ts
+++ src/lib/generateJWTAndSetCookies.ts
@@ -7,12 +7,11 @@ export function generateJWTAndSetCookies(userId: string, res: Response) {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET!, {
     expiresIn: "30d",
   });
-
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    // domain: process.env.DOMAIN,
+    sameSite: "lax",
+    // sameSite: process.env.NODE_ENV === "production" ? "" : "lax",
     path: "/",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
